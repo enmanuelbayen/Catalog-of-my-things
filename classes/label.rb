d8e73648e08980a6b815df048b1a60b914073f5a
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-08 17:42:07 +0300

    Fix linters

--- classes/label.rb
+++ classes/label.rb
@@ -1,14 +1,13 @@
 class Label
-    attr_accessor :name, :items
-  
-    def initialize(name)
-      @name = name
-      @items = []
-    end
-  
-    def add_item(item)
-      item.add_label(self)
-      @items << item
-    end
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @name = name
+    @items = []
   end
-  
+
+  def add_item(item)
+    item.add_label(self)
+    @items << item
+  end
+end
