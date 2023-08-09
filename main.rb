57342b2bd1c5da7a64edcdeabe0588fac67ffbb1
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-09 23:44:03 +0300

    Update main file

--- main.rb
+++ main.rb
@@ -16,22 +16,6 @@ class Main
     @app.save_data
     puts 'Thank you for using our application.'
   end
-
-  def display_actions
-    puts
-    puts '1- List all music albums'
-    puts '2- List all books'
-    puts '3- List of games'
-    puts '4- List all genres'
-    puts '5- List all labels'
-    puts '6- List all authors'
-    puts '7- Add a book'
-    puts '8- Add a music album'
-    puts '9- Add a game'
-    puts '10- Add a movie'
-    puts '11- Add a game'
-    puts '12- Exit'
-  end
 end
 
 Main.new.run
