2a6e8ac7f52f42269a3ea0ea7a93e9537234c213
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-08-11 16:41:03 +0300

    Fix label test

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,18 +1,31 @@
-require_relative '../classes/label'
+require_relative '../classes/label'  # Adjust the path accordingly
 
-describe Label do
-  let(:label_data) do
-    {
-      id: 456,
-      title: 'Label Title',
-      color: 'Label Color',
-      items: [] # Include an empty array for the items key
-    }
+RSpec.describe Label do
+  let(:dummy_item) { double('item') }
+
+  describe '#initialize' do
+    it 'initializes a label object with title, color, and optional id' do
+      label = Label.new('Sample Title', 'Red', 123)
+      expect(label.title).to eq('Sample Title')
+      expect(label.color).to eq('Red')
+      expect(label.id).to eq(123)
+    end
   end
 
-  it 'can be converted to hash' do
-    label = Label.new(**label_data)
-    label_hash = label.to_h
-    expect(label_hash).to eq(label_data)
+  describe '.list_all_labels' do
+    it 'lists all labels' do
+      labels = [
+        Label.new('Label 1', 'Red', 123),
+        Label.new('Label 2', 'Blue', 456)
+      ]
+
+      expect { Label.list_all_labels(labels) }.to output(/Label 1.*Label 2/m).to_stdout
+    end
+
+    it 'outputs a message for empty label list' do
+      labels = []
+
+      expect { Label.list_all_labels(labels) }.to output(/No labels available/).to_stdout
+    end
   end
 end
