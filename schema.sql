e390694ebe926f619cc752afcaa218a0457853e3
enmanuelbayen <enmanuel0495@gmail.com>
2023-08-11 11:13:17 -0400

    schema for item, genre and music

--- schema.sql
+++ schema.sql
@@ -4,6 +4,17 @@ CREATE TABLE labels (
     color TEXT NOT NULL
 );
 
+CREATE TABLE genres (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL
+);
+
 CREATE TABLE books (
     id INTEGER PRIMARY KEY,
     publisher TEXT NOT NULL,
@@ -19,5 +30,20 @@ CREATE TABLE genres_items_books (
     book_id INTEGER NOT NULL,
     genre_id INTEGER NOT NULL,
     FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
+CREATE TABLE music_albums (
+    id INTEGER PRIMARY KEY,
+    on_spotify BOOLEAN NOT NULL,
+    item_id INTEGER NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES items(id)
+);
+
+CREATE TABLE genres_items_music_albums (
+    id INTEGER PRIMARY KEY,
+    music_album_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    FOREIGN KEY (music_album_id) REFERENCES music_albums(id),
     FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
